718f84196f03ec012034ffa33b247b8f75b8ceac
Joao <joaopedrosouzarodri@gmail.com>
2025-09-03 16:27:27 -0300

    feat: add findOne abstract method

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,6 +1,6 @@
-import { Logger } from "@nestjs/common";
+import { Logger, NotFoundException } from "@nestjs/common";
 import { AbstractDocument } from "./abstract.schema";
-import { Model, Types } from "mongoose";
+import { FilterQuery, Model, Types, UpdateQuery } from "mongoose";
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     protected abstract readonly logger:  Logger;
@@ -13,4 +13,18 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
         });
         return (await createdDocument.save()).toJSON() as unknown as TDocument
     }
+
+    async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument>{
+        const document = await this.model
+        .findOne(filterQuery)
+        .lean<TDocument>(true)
+
+        if(!document){
+            this.logger.warn('Document was not found with filterQuery', filterQuery)
+            throw new NotFoundException('Documento was not found')
+        }
+
+        return document;
+    }
+
 }
